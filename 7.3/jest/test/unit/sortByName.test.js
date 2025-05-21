ed034c705877a6d12f4661a9ec31349db453f742
Alina <eparis@atlassian.com>
2025-05-21 12:55:45 +0300

    jest task

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,34 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+  });
+
+  it("Books names should remain in the same order if equal", () => {
+    const input = ["Властелин Колец", "Властелин Колец", "Властелин Колец"];
+
+    const expected = ["Властелин Колец", "Властелин Колец", "Властелин Колец"];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+  });
+
+  it("Without params throws exception", () => {
+    expect(() => sorting.sortByName()).toThrow(TypeError);
   });
 });
